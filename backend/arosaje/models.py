cd366869aa937ba527e5d3b009217d0a02838ab3
graphyke <gueretlouis13@gmail.com>
2025-04-04 10:20:04 +0200

    [ADD] model constraint, serializer

--- backend/arosaje/models.py
+++ backend/arosaje/models.py
@@ -3,6 +3,7 @@ from address.models import AddressField
 
 from datetime import datetime
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 class Species(models.Model):
@@ -28,10 +29,49 @@ class Commentaries(models.Model):
 
 class Posts(models.Model):
     commentary = models.TextField()
-    start_of_event = models.DateTimeField()
-    end_of_event = models.DateTimeField()
-    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_of_event = models.DateField()
+    end_of_event = models.DateField()
     plant = models.ForeignKey(Plants, on_delete=models.CASCADE)
+
+    def clean(self):
+        super().clean()
+        #modify to a SQL constraint when migrating to postgres
+        if self.start_of_event <= datetime.now().date():
+            raise ValidationError(
+                "starting date of a post must be superior to today's date."
+            )
+
+        post_from_plant = Posts.objects.filter(plant=self.plant)
+        overlap = post_from_plant.filter(
+            start_of_event__lte=self.end_of_event,
+            end_of_event__gte=self.end_of_event
+        ) | post_from_plant.filter(
+            start_of_event__lte=self.start_of_event,
+            end_of_event__gte=self.start_of_event
+        ) | post_from_plant.filter(
+            start_of_event__gte=self.start_of_event,
+            end_of_event__lte=self.end_of_event
+        )
+        
+        if self.pk:
+            overlap = overlap.exclude(pk=self.pk)
+        
+        if overlap.exists():
+            raise ValidationError(
+                "There is already a period for this plant that overlap specified dates."
+            )
+    
+    def save(self, *args, **kwargs):
+        self.full_clean()  # Assure que la validation est toujours exécutée
+        return super().save(*args, **kwargs)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(end_of_event__gt=models.F('start_of_event')),
+                name='start_before_end'
+            ),
+        ]
 
 class Keeping(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="keeping_user")
---
+++ backend/arosaje/tests/test_postTestCase.py
@@ -0,0 +1,129 @@
+import datetime
+
+from address.models import Address
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+from django.utils import timezone
+
+from arosaje.models import Plants, Posts, Species 
+
+class PostsTestCase(TestCase):
+    def setUp(self):
+
+        self.user = User.objects.create_user(
+            username='johndoe',
+            email='john@example.com',
+            password='secure_password123'
+        )
+
+        self.address = Address.objects.create(
+            raw="Parc de haute technologie du Moulin Leblanc, Campu Sup, 08000 Charleville-Mézières"
+        )
+
+        # Créer une plante pour les tests
+        self.species = Species.objects.create(
+            name="Rose",
+            water_dependency=2,
+            light_dependency=3,
+        )
+
+        self.plant = Plants.objects.create(
+            species=self.species,
+            owner=self.user,
+            address=self.address,
+            picture="/media/hortensia.png"
+        )
+        
+        # Créer une période existante
+        self.post = Posts.objects.create(
+            commentary="origin post",
+            plant=self.plant,
+            start_of_event=timezone.now().date() + datetime.timedelta(days=5),
+            end_of_event=timezone.now().date() + datetime.timedelta(days=8)
+        )
+
+    def test_with_non_overlapping_after_existing_periods(self):
+        new_post = Posts(
+            commentary="cool post",
+            plant=self.plant,
+            start_of_event=timezone.now().date() + datetime.timedelta(days=9),
+            end_of_event=timezone.now().date() + datetime.timedelta(days=10)
+        )
+
+        try:
+            new_post.save()
+        except ValidationError:
+            self.fail("post with non overlappings periods are not inserted")
+
+        self.assertTrue(True)
+
+    def test_with_non_overlapping_before_existing_periods(self):
+        new_post = Posts(
+            commentary="cool post",
+            plant=self.plant,
+            start_of_event=timezone.now().date() + datetime.timedelta(days=1),
+            end_of_event=timezone.now().date() + datetime.timedelta(days=4)
+        )
+
+        try:
+            new_post.save()
+        except ValidationError:
+            self.fail("post with non overlappings periods are not inserted")
+
+        self.assertTrue(True) 
+
+    def test_with_overlapping_start_of_event(self):
+        new_post = Posts(
+            commentary="cool post",
+            plant=self.plant,
+            start_of_event=timezone.now().date() + datetime.timedelta(days=6),
+            end_of_event=timezone.now().date() + datetime.timedelta(days=10)
+        )
+
+        with self.assertRaises(ValidationError):
+            new_post.save()
+
+    def test_with_overlapping_end_of_event(self):
+        new_post = Posts(
+            commentary="cool post",
+            plant=self.plant,
+            start_of_event=timezone.now().date() + datetime.timedelta(days=2),
+            end_of_event=timezone.now().date() + datetime.timedelta(days=6)
+        )
+
+        with self.assertRaises(ValidationError):
+            new_post.save()
+    
+    def test_with_overlapping_start_and_end_of_event(self):
+        new_post = Posts(
+            commentary="cool post",
+            plant=self.plant,
+            start_of_event=timezone.now().date() + datetime.timedelta(days=6),
+            end_of_event=timezone.now().date() + datetime.timedelta(days=7)
+        )
+
+        with self.assertRaises(ValidationError):
+            new_post.save()
+    
+    def test_with_wrapping_period(self):
+        new_post = Posts(
+            commentary="cool post",
+            plant=self.plant,
+            start_of_event=timezone.now().date() + datetime.timedelta(days=2),
+            end_of_event=timezone.now().date() + datetime.timedelta(days=10)
+        )
+
+        with self.assertRaises(ValidationError):
+            new_post.save()
+    
+    def test_with_starting_date_to_today(self):
+        new_post = Posts(
+            commentary="cool post",
+            plant=self.plant,
+            start_of_event=timezone.now().date(),
+            end_of_event=timezone.now().date() + datetime.timedelta(days=2)
+        )
+
+        with self.assertRaises(ValidationError):
+            new_post.save()
