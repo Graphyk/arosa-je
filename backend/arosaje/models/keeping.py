0b719474459586747fb7a749cbb26e1dec1bb51b
graphyke <gueretlouis13@gmail.com>
2025-05-12 11:27:40 +0200

    [ADD] create keeping

--- backend/arosaje/models/keeping.py
+++ backend/arosaje/models/keeping.py
@@ -1,8 +1,34 @@
+from datetime import datetime
+
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from .posts import Posts
 
 class Keeping(models.Model):
+    STATUSES = [
+        (0, "pending"),
+        (1, "canceled"),
+        (2, "not taken"),
+        (3, "ongoing"),
+        (4, "validated"),
+        (5, "not gave back")
+    ]
+
+    pk = models.CompositePrimaryKey("keeper_id", "post_id")
     keeper = models.ForeignKey(User, on_delete=models.CASCADE, related_name="keeping_keeper")
     post = models.ForeignKey(Posts, on_delete=models.CASCADE)
+    _status = models.SmallIntegerField(choices=STATUSES, default=0)
+
+    @property
+    def status(self):
+        return self._status
+
+    def clean(self):
+        super().clean()
+
+        if (self.post.start_of_event <= datetime.now().date()):
+            raise ValidationError(
+                "you can't accept a keeping at the last moment"
+            )
---
+++ backend/arosaje/tests/test_keeping.py
@@ -0,0 +1,80 @@
+import datetime
+
+from address.models import Address
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+from django.utils import timezone
+from unittest.mock import patch
+
+from arosaje.models import Plants, Posts, Species, Keeping
+
+class KeepingTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='johndoe',
+            email='john@example.com',
+            password='secure_password123'
+        )
+
+        self.address = Address.objects.create(
+            raw="Parc de haute technologie du Moulin Leblanc, Campu Sup, 08000 Charleville-Mézières"
+        )
+
+        # Créer une plante pour les tests
+        self.species = Species.objects.create(
+            name="Rose",
+            water_dependency=2,
+            light_dependency=3,
+        )
+
+        self.plant = Plants.objects.create(
+            species=self.species,
+            owner=self.user,
+            address=self.address,
+            picture="/media/hortensia.png"
+        )
+
+        self.keeper = User.objects.create_user(
+            username='johndeer',
+            email='johndeer@example.com',
+            password='secure_password123'
+        )
+        
+        self.post = Posts.objects.create(
+            commentary="origin post",
+            plant=self.plant,
+            start_of_event=timezone.now().date() + datetime.timedelta(days=5),
+            end_of_event=timezone.now().date() + datetime.timedelta(days=8)
+        )
+
+    def test_insert_keeping_before_start_of_event(self):
+        keeping = Keeping(
+            keeper=self.keeper,
+            post=self.post
+        )
+
+        keeping.save()
+        self.assertEqual(keeping.status, 0)
+
+    def test_insert_keeping_same_day_than_start_of_event(self):
+        # don't save because it will raise a validation error, but you normally need to save an object after editing it
+        self.post.start_of_event = timezone.now().date()
+
+        keeping = Keeping(
+            keeper=self.keeper,
+            post=self.post
+        )
+        with self.assertRaises(ValidationError):
+            keeping.save()
+    
+    def test_insert_keeping_after_start_of_event(self):
+        # don't save because it will raise a validation error, but you normally need to save an object after editing it
+        self.post.start_of_event = timezone.now().date() - datetime.timedelta(days=1)
+
+        keeping = Keeping(
+            keeper=self.keeper,
+            post=self.post
+        )
+        with self.assertRaises(ValidationError):
+            keeping.save()
