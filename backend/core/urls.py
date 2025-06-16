00cd4ba27d8eb65c2239dba68e17cfb103a99509
graphyke <gueretlouis13@gmail.com>
2025-06-16 13:58:06 +0200

    Revert "[FIX] delete sveltekit"
    This reverts commit e4e19d4715915b70294e677e44e59c73656ffeda.

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import include, path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework import routers
 
 from arosaje import views
@@ -32,7 +31,5 @@ apiRouter.register(r'keeping', KeepingViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('api/', include(apiRouter.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh")
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
