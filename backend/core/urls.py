d5c38013692d06be64c039d73cdb74d11e44ee61
graphyke <gueretlouis13@gmail.com>
2025-06-17 10:12:40 +0200

    Reapply "typo"
    This reverts commit 4f3cd14bbffb915a753cd49cdb87acb3ea56aa80.

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -36,3 +39,6 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh")
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
