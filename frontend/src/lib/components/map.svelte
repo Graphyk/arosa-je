eaa403724208487156fbbdf85b572e76d71fbf53
graphyke <gueretlouis13@gmail.com>
2025-06-23 13:46:53 +0200

    [IMP] get plants without sending precise lat and lon

--- frontend/src/lib/components/map.svelte
+++ frontend/src/lib/components/map.svelte
@@ -2,7 +2,7 @@
     import { getContext, onMount } from 'svelte';
     import plantPin from '$lib/icon/pin/plantPin';
 
-    import { map as createMap, latLng, tileLayer, type MapOptions, marker, type MarkerOptions, type LeafletMouseEvent, LatLng} from 'leaflet';
+    import { map as createMap, latLng, tileLayer, type MapOptions, marker, type MarkerOptions, type LeafletMouseEvent, LatLng, circle, Circle} from 'leaflet';
     import type { DataProvider } from '$lib/type/dataProvider';
     import getUserLocation from '$lib/utils/getUserLocation';
     import type { Plant } from '$lib/type/resources/plant';
@@ -17,11 +17,37 @@
     }: Props = $props(); 
 
     let mapDiv: HTMLElement;
+    let radius = 500;
+
+    let currentRange: Circle<any>
 
     const dataProvider = getContext<DataProvider>("dataProvider");
 
-    function onMapClick(e: LeafletMouseEvent) {
-      console.log(e);
+    async function onMapClick(e: LeafletMouseEvent) {
+      if (currentRange) {
+        currentRange.remove();
+      }
+      const plantsInRange = await dataProvider.getCount("api/plants", {
+        filter: {
+          center_lat: e.latlng.lat,
+          center_lng: e.latlng.lng,
+          radius: radius
+        }
+      });
+      currentRange = circle(e.latlng, {
+        color: '#4CAF50',
+        fillColor: '#4CAF50',
+        fillOpacity: 0.2,
+        radius: radius
+      }).addTo(e.target);
+
+
+      currentRange.bindTooltip(plantsInRange.toString(), {
+        permanent: true,
+        direction: 'center',
+        className: '!bg-primary-600'
+      })
+
     }
     
     function onPinClick(e: LeafletMouseEvent) {
@@ -33,32 +59,23 @@
     }
     
     onMount(async () => {
+      const baseLocation = await getUserLocation() ?? latLng(40.731253, -73.996139);
+
       let options: MapOptions = {
-        center: await getUserLocation() ?? latLng(40.731253, -73.996139),
+        center: baseLocation,
         zoom: 16,
       };
 
       const map = createMap(mapDiv, options);
       map.on('click', onMapClick);
 
+      const newMarker = marker(baseLocation, options)
+      newMarker.addTo(map);
+
       tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       }).addTo(map);
 
-      dataProvider.getList("api/plants").then(res => {
-        res.data.forEach(element => {
-          const options: plantMarkerOptions = {
-            icon: plantPin, 
-            dataPlant: element
-          }
-  
-          const newMarker = marker(latLng(element.lat, element.lon), options)
-
-          newMarker.on("click", onPinClick);
-          
-          newMarker.addTo(map);
-        })
-      })
   });
 </script>
 
